212c4be2f41d3393d884914e1c9e2a09307df9ba
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 21:07:52 +0600

    sum of number from 1 to 49 that are divisible by 3

--- even.js
+++ even.js
@@ -1,18 +1,30 @@
 // sum of even numbers using for loop 
 let sum = 0;
-for (let i = 0; i < 20;  i+=2 ){
+for (let i = 0; i < 20; i += 2) {
     console.log(i)
     sum = i + sum;
 }
-console.log('sum of even numbers are:',sum)
+console.log('sum of even numbers are:', sum)
 
 // sum of odd numbers using for loop 
 
-let sum = 0;
-for(let i=0; i <=21; i++){
-    if (i % 2 == 1 ){
+let summation = 0;
+for (let i = 0; i <= 21; i++) {
+    if (i % 2 == 1) {
         console.log(i)
-        sum = sum + i;
+        summation = summation + i;
     }
 }
-console.log('sum of odd numbers are:',sum)
+console.log('sum of odd numbers are:', sum)
+
+//sum of number from 1 to 49 that are divisible by 3
+
+let total = 0;
+for (i = 1; i<=49; i++){
+    if(i % 3 === 0){
+        console.log(i)
+        total = total + i;
+        console.log('summation:',summation)
+    }
+}
+console.log('total :',total)
